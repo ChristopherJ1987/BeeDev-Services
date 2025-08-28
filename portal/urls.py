ccbec1da1b2e27ec6a937133ce28e8d10b0235eb
melissa-24 <mlongenberger@codingdojo.com>
2025-08-28 15:37:48 -0400

    small changes and started the migrations

--- portal/urls.py
+++ portal/urls.py
@@ -4,6 +4,7 @@ from userApp import views as app_views
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
+    path('__reload__/', include('django_browser_reload.urls')),
     path('', include('userApp.urls')),
     path('admin/', admin.site.urls),
 ]
