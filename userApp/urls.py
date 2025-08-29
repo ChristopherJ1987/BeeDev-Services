9e32716a16e246ed882d27ca1c9ed0e18c05b78f
melissa-24 <mlongenberger@codingdojo.com>
2025-08-29 14:10:08 -0400

    currently not working right

--- userApp/urls.py
+++ userApp/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from .views import PortalLogin, post_login, staff_home, client_home
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth.views import LogoutView
 
 # All urls are at base /
 
@@ -13,6 +14,7 @@ urlpatterns = [
     path("post-login/", post_login, name="post_login"),
     path("staff/", staff_home, name="staff_home"),
     path("client/", client_home, name="client_home"),
+    path("logout/", LogoutView.as_view(next_page="userApp:login"), name="logout"),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
