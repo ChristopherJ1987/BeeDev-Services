8693ba6ec697f25abb9f358b20795e98e7755b50
melissa-24 <mlongenberger@codingdojo.com>
2025-09-11 13:08:08 -0400

    fixed render issue still working on logout issue

--- userApp/views/root.py
+++ userApp/views/root.py
@@ -1,16 +1,15 @@
 from django.contrib.auth.views import LoginView
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from core.utils.context import CommonContextMixin, base_ctx
+from ..forms import PortalAuthForm
 
-class PortalLogin(LoginView):
+class PortalLogin(CommonContextMixin,LoginView):
     template_name = "userApp/index.html"
     redirect_authenticated_user = True
+    form_class = PortalAuthForm
+    common_title = "Portal"
 
-    # You can add your custom context just like your index() example:
-    def get_context_data(self, **kwargs):
-        ctx = super().get_context_data(**kwargs)
-        ctx = {"title": "Portal"}
-        return ctx
 
 @login_required
 def post_login(request):
@@ -29,7 +28,7 @@ def staff_home(request):
 def client_home(request):
     u = request.user
     ctx = {
-        "title": "Dashboard - BeeDev Services",
         "user_name": u.get_full_name() or u.username,
     }
+    ctx.update(base_ctx(request, title="Dashboard"))
     return render(request, "userApp/client_home.html", ctx)
